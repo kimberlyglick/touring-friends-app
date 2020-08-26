788cfc7b639ab1a42c7d6120ca1f718e396cb25b
Kimberly_Glick <kimberlyjglick@gmail.com>
2020-08-26 18:10:54 -0400

    Changed landing page.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import bus from './design/bus.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body className="App-body">
+        <img src={bus} className="Header-image"/>
+          <p>
+            Kimberly and Emily Make something together!
+          </p>
+      </body>
     </div>
   );
 }
