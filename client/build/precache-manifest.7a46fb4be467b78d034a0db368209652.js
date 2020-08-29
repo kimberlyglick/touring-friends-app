8052ee72607699040ccdfe1a1cda3b7d8aa9af1a
Em Ekdahl <emily.ekdahl@gmail.com>
2020-08-29 05:36:24 -0500

    reorganizing

---
+++ client/build/precache-manifest.7a46fb4be467b78d034a0db368209652.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babf9f72e90ecb051aeae0ec9717481f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5600e96b297d24052924",
+    "url": "/static/css/main.fde3b262.chunk.css"
+  },
+  {
+    "revision": "e8da6f2f99707f9e5b73",
+    "url": "/static/js/2.b854b2b1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b854b2b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5600e96b297d24052924",
+    "url": "/static/js/main.4d88118d.chunk.js"
+  },
+  {
+    "revision": "8759f81ab0eeb79c32a2",
+    "url": "/static/js/runtime-main.0bd6282d.js"
+  },
+  {
+    "revision": "a9b6db9a6756a8bbd2fe1a1c102b401d",
+    "url": "/static/media/IMG_2578.a9b6db9a.jpeg"
+  },
+  {
+    "revision": "0c369efb668026b43514123d85d73006",
+    "url": "/static/media/bus.0c369efb.jpg"
+  }
+]);
--- client/src/index.js
+++ client/src/index.js
@@ -15,3 +15,10 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
